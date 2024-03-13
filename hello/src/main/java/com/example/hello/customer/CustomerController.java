f852d0dd1913c609ff4d15b3d59718441e35c895
up1 <somkiat.p@gmail.com>
2024-03-13 16:16:33 +0700

    Testing with Post

--- hello/src/main/java/com/example/hello/customer/CustomerController.java
+++ hello/src/main/java/com/example/hello/customer/CustomerController.java
@@ -1,7 +1,6 @@
 package com.example.hello.customer;
 
 import jakarta.validation.Valid;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
--- hello/src/test/java/com/example/hello/customer/CustomerControllerTest.java
+++ hello/src/test/java/com/example/hello/customer/CustomerControllerTest.java
@@ -8,9 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.ResponseEntity;
 
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class CustomerControllerTest {
@@ -53,4 +51,21 @@ class CustomerControllerTest {
         assertEquals(500, response.getStatusCode().value());
         assertEquals("Error", response.getBody().getMessage());
     }
+
+    @Test
+    @DisplayName("Success case  :: create a new customer")
+    public void createNewCustomer(){
+        // Arrange
+        CustomerCreateRequest request = new CustomerCreateRequest();
+        request.setFname("somkiat");
+        request.setLname("pui");
+        // Act
+        ResponseEntity<CustomerResponse> response
+                = restTemplate.postForEntity("/customer", request, CustomerResponse.class);
+        // Assert
+        assertEquals(201, response.getStatusCode().value());
+        assertEquals(123, response.getBody().getId());
+        assertEquals("somkiat pui", response.getBody().getFull_name());
+    }
+
 }

808956eba61f5269a204a197f960846c315a67f3
up1 <somkiat.p@gmail.com>
2024-03-13 11:33:50 +0700

    Remove duplication

--- hello/src/main/java/com/example/hello/customer/CustomerController.java
+++ hello/src/main/java/com/example/hello/customer/CustomerController.java
@@ -2,17 +2,19 @@ package com.example.hello.customer;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/customer")
 public class CustomerController {
 
-    @GetMapping("/customer")
+    @GetMapping("")
     public String getAllCustomer(){
         return "Hello Get all customer";
     }
 
-    @GetMapping("/customer/{id}")
+    @GetMapping("/{id}")
     public String getCustomerById(@PathVariable int id) {
         return "Get customer by id=" + id;
     }

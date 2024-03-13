3142e16c4a1a4bd188e6b1d3542613dddeed7c99
up1 <somkiat.p@gmail.com>
2024-03-13 11:55:53 +0700

    Add test case

---
+++ hello/src/test/java/com/example/hello/customer/CustomerControllerTest.java
@@ -0,0 +1,34 @@
+package com.example.hello.customer;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+class CustomerControllerTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    @DisplayName("ทำการทดสอบดึงข้อมูล customer ทั้งหมดขึ้นมาดู ต้อง success นะ")
+    void getAllCustomer() {
+        // Act
+        String response = restTemplate.getForObject("/customer", String.class);
+        // Assert
+        assertEquals("Hello Get all customer", response);
+    }
+
+    @Test
+    void getCustomerById() {
+        // Act
+        String response = restTemplate.getForObject("/customer/1", String.class);
+        // Assert
+        assertEquals("Get customer by id=1", response);
+    }
+}

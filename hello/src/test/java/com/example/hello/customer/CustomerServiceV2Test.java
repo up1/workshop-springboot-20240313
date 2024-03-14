daf7df3e16fdd84f760a94a7f42bfa151fc2398f
up1 <somkiat.p@gmail.com>
2024-03-14 14:29:14 +0700

    Service testing with SpringBootTest

---
+++ hello/src/test/java/com/example/hello/customer/CustomerServiceV2Test.java
@@ -0,0 +1,30 @@
+package com.example.hello.customer;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.boot.test.context.SpringBootTest.*;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+class CustomerServiceV2Test {
+
+    @Autowired
+    private CustomerService customerServiceV2;
+
+    @Test
+    @DisplayName("Success with valid input")
+    void case01() {
+        // Arrange
+        CustomerCreateRequest request = new CustomerCreateRequest();
+        request.setFname("somkiat");
+        request.setLname("pui");
+        // Act
+        CustomerResponse response = customerServiceV2.process(request);
+        // Assert
+        assertTrue(response.getId() > 0);
+        assertEquals("somkiat pui", response.getFull_name());
+    }
+}

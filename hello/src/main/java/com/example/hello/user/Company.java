80fa4070bf15c36643d94fe78efb2920075919b5
up1 <somkiat.p@gmail.com>
2024-03-14 15:37:15 +0700

    Create user gateway to call data from jsonplaceholder.com

---
+++ hello/src/main/java/com/example/hello/user/Company.java
@@ -0,0 +1,41 @@
+package com.example.hello.user;
+
+public class Company{
+	private String bs;
+	private String catchPhrase;
+	private String name;
+
+	public void setBs(String bs){
+		this.bs = bs;
+	}
+
+	public String getBs(){
+		return bs;
+	}
+
+	public void setCatchPhrase(String catchPhrase){
+		this.catchPhrase = catchPhrase;
+	}
+
+	public String getCatchPhrase(){
+		return catchPhrase;
+	}
+
+	public void setName(String name){
+		this.name = name;
+	}
+
+	public String getName(){
+		return name;
+	}
+
+	@Override
+ 	public String toString(){
+		return 
+			"Company{" + 
+			"bs = '" + bs + '\'' + 
+			",catchPhrase = '" + catchPhrase + '\'' + 
+			",name = '" + name + '\'' + 
+			"}";
+		}
+}
---
+++ hello/src/main/java/com/example/hello/user/UserGateway.java
@@ -0,0 +1,21 @@
+package com.example.hello.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Optional;
+
+@Component
+public class UserGateway {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public Optional<UserResponse> callUserById(int id) {
+        String url = "https://jsonplaceholder.typicode.com/users/1";
+        UserResponse response = restTemplate.getForObject(url, UserResponse.class);
+        return  Optional.of(response);
+    }
+
+}

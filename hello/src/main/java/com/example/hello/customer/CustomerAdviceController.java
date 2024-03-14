f8db3b5e5b795f695dcacb17675150782a5720ef
up1 <somkiat.p@gmail.com>
2024-03-14 09:38:28 +0700

    Working with validator

--- hello/src/main/java/com/example/hello/customer/CustomerAdviceController.java
+++ hello/src/main/java/com/example/hello/customer/CustomerAdviceController.java
@@ -2,8 +2,12 @@ package com.example.hello.customer;
 
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
 
 @RestControllerAdvice
 public class CustomerAdviceController {
@@ -20,6 +24,17 @@ public class CustomerAdviceController {
         ErrorResponse errorResponse = new ErrorResponse();
         errorResponse.setMessage(e.getMessage());
         return new ResponseEntity<>(errorResponse, HttpStatusCode.valueOf(500));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> xyz(MethodArgumentNotValidException e) {
+        ErrorResponse errorResponse = new ErrorResponse();
+        List<FieldError> errors = e.getBindingResult().getFieldErrors();
+        for (FieldError error : errors) {
+            System.out.println(error.getDefaultMessage());
+            errorResponse.setMessage(error.getDefaultMessage());
+        }
+        return new ResponseEntity<>(errorResponse, HttpStatusCode.valueOf(200));
     }
 
 }
--- hello/src/main/java/com/example/hello/customer/CustomerCreateRequest.java
+++ hello/src/main/java/com/example/hello/customer/CustomerCreateRequest.java
@@ -1,8 +1,6 @@
 package com.example.hello.customer;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public class CustomerCreateRequest{

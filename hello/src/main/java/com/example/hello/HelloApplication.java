f1c75c69619c5d130cabebb6073a490b3849ef1b
up1 <somkiat.p@gmail.com>
2024-03-14 10:08:01 +0700

    List of beans

--- hello/src/main/java/com/example/hello/HelloApplication.java
+++ hello/src/main/java/com/example/hello/HelloApplication.java
@@ -4,12 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
+import java.util.Arrays;
+
 @SpringBootApplication
 public class HelloApplication {
 
 	public static void main(String[] args) {
 		 ConfigurableApplicationContext context
 				 = SpringApplication.run(HelloApplication.class, args);
+		 String[] beans = context.getBeanDefinitionNames();
+        Arrays.stream(beans).forEach(System.out::println);
 		System.out.println(context.getBeanDefinitionCount());
 	}
 
